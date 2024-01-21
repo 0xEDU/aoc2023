06a79c92ed8463f7c2466ef340b3457d26204e5b
edu <bfwaend@gmail.com>
2024-01-21 01:44:11 -0300

    Solve Day 1 part 1

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"unicode"
 )
 
 func main() {
-	fmt.Println("Hello, Seamen!")
+	f, err := os.Open("./cmd/day1/input")
+	if err != nil {
+		panic(err)
+	}
+	finalNumber := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		finalNumber += getNumberFromLine([]rune(scanner.Text()))
+	}
+	fmt.Println(finalNumber)
+}
+
+func getNumberFromLine(text []rune) int {
+	var first rune
+	var last rune
+	for _, c := range text {
+		if unicode.IsDigit(c) {
+			first = c
+			break
+		}
+	}
+	for i := len(text) - 1; i >= 0; i-- {
+		if unicode.IsDigit(text[i]) {
+			last = text[i]
+			break
+		}
+	}
+	finalString := fmt.Sprintf("%c%c", first, last)
+	result, err := strconv.Atoi(finalString)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
