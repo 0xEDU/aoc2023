4dac5d9a103ab2c3015ea8d62ae6ed18fe1bdeb3
edu <bfwaend@gmail.com>
2024-01-23 13:09:49 -0300

    Solve day2 part 1

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -75,6 +75,31 @@ func appendGameToList(head **Game, game **Game) {
 	temp.next = *game
 }
 
+func sliceHasLargerValue(colorSlice []int, value int) bool {
+	for _, sliceValue := range colorSlice {
+		if sliceValue > value {
+			return true
+		}
+	}
+	return false
+}
+
+func getSumOfValidGames(list *GameList) int {
+	sumOfValidGames := 0
+	temp := (*list).head
+	validValues := []int{temp.reds[0], temp.greens[0], temp.blues[0]}
+	temp = temp.next
+	for temp != nil {
+		if sliceHasLargerValue(temp.reds, validValues[0]) || sliceHasLargerValue(temp.greens, validValues[1]) || sliceHasLargerValue(temp.blues, validValues[2]) {
+			temp = temp.next
+			continue
+		}
+		sumOfValidGames += temp.id
+		temp = temp.next
+	}
+	return sumOfValidGames
+}
+
 func main() {
 	f := file.Open("./cmd/day2/input")
 	gameList := GameList{nil}
@@ -83,5 +108,7 @@ func main() {
 		game := makeGameFromLine(f.LineContent())
 		appendGameToList(&gameList.head, &game)
 	}
+	sumOfValidGames := getSumOfValidGames(&gameList)
+	fmt.Println(sumOfValidGames)
 	f.Close()
 }
