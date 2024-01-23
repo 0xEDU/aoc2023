f7a009e4f94e08f1b20070d101820efa0d1e0ecf
edu <bfwaend@gmail.com>
2024-01-23 02:01:19 -0300

    Solve Day 1 part 2

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -21,18 +22,40 @@ func main() {
 	fmt.Println(finalNumber)
 }
 
+func hasAnyNumberPrefix(text string) (bool, string, string) {
+	prefixes := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	substitutes := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	for i, prefix := range prefixes {
+		if strings.HasPrefix(text, prefix) {
+			return true, prefix, substitutes[i]
+		}
+	}
+	return false, "", ""
+}
+
+func findNumber(text string, c rune, i int) rune {
+	textSlice := text[i:]
+	if unicode.IsDigit(c) {
+		return c
+	}
+	result, prefix, substitute := hasAnyNumberPrefix(textSlice)
+	if result {
+		textSlice = strings.Replace(textSlice, prefix, substitute, 1)
+		return rune(textSlice[0])
+	}
+	return 0
+}
+
 func getNumberFromLine(text []rune) int {
-	var first rune
-	var last rune
-	for _, c := range text {
-		if unicode.IsDigit(c) {
-			first = c
+	var first, last rune
+	textString := string(text)
+	for i, c := range textString {
+		if first = findNumber(textString, c, i); first != 0 {
 			break
 		}
 	}
-	for i := len(text) - 1; i >= 0; i-- {
-		if unicode.IsDigit(text[i]) {
-			last = text[i]
+	for i := len(textString) - 1; i >= 0; i-- {
+		if last = findNumber(textString, text[i], i); last != 0 {
 			break
 		}
 	}
