e9215765eae2299eec621843f31e10808445cdf2
edu <bfwaend@gmail.com>
2024-01-23 12:57:18 -0300

    Save all games in a linked list

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -20,10 +20,10 @@ type GameList struct {
 	head *Game
 }
 
-func getGameId(gameIdString string) int {
+func extractNumberFromString(str string) int {
 	var builder strings.Builder
 
-	for _, c := range gameIdString {
+	for _, c := range str {
 		if unicode.IsDigit(c) {
 			builder.WriteRune(c)
 		}
@@ -32,23 +32,47 @@ func getGameId(gameIdString string) int {
 	return result
 }
 
+func getGameSetsFromSequence(sequence string) ([]int, []int, []int) {
+	var reds, greens, blues []int
+
+	splitSequence := strings.Split(sequence, ";")
+	fmt.Println(splitSequence)
+	for _, sequence := range splitSequence {
+		colors := strings.Split(sequence, ",")
+		for _, color := range colors {
+			if strings.Contains(color, "red") {
+				reds = append(reds, extractNumberFromString(color))
+			}
+			if strings.Contains(color, "green") {
+				greens = append(greens, extractNumberFromString(color))
+			}
+			if strings.Contains(color, "blue") {
+				blues = append(blues, extractNumberFromString(color))
+			}
+		}
+	}
+	fmt.Println("Reds:", reds, "Greens:", greens, "Blues:", blues)
+	return reds, greens, blues
+}
+
 func makeGameFromLine(line string) *Game {
 	game := new(Game)
 
 	splitString := strings.Split(line, ":")
 	gameIdString := splitString[0]
 	gameSetsSequence := splitString[1]
-	game.id = getGameId(gameIdString)
+	game.id = extractNumberFromString(gameIdString)
 	game.reds, game.greens, game.blues = getGameSetsFromSequence(gameSetsSequence)
+	game.next = nil
 	return game
 }
 
-func getGameSetsFromSequence(sequence string) ([]int, []int, []int) {
-	var reds, greens, blues []int
-
-	splitSequence := strings.Split(sequence, ";")
-	fmt.Println(splitSequence)
-	return reds, greens, blues
+func appendGameToList(head **Game, game **Game) {
+	temp := *head
+	for temp.next != nil {
+		temp = temp.next
+	}
+	temp.next = *game
 }
 
 func main() {
@@ -56,7 +80,8 @@ func main() {
 	gameList := GameList{nil}
 	gameList.head = &Game{0, []int{12}, []int{13}, []int{14}, nil}
 	for f.GetLine() {
-		makeGameFromLine(f.LineContent())
+		game := makeGameFromLine(f.LineContent())
+		appendGameToList(&gameList.head, &game)
 	}
 	f.Close()
 }
